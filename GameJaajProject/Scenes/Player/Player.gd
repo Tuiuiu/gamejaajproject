d001e455d6f81bb1013f965a4c2aac429fbae481
Andre Kato <andrekato@usp.br>
2020-12-26 19:51:28 -0300

    Adds double jump and enlarges player platform

--- GameJaajProject/Scenes/Player/Player.gd
+++ GameJaajProject/Scenes/Player/Player.gd
@@ -4,22 +4,27 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var GRAVITY = 4000.0
-var JUMP_FORCE = 1000.0
+var GRAVITY = 50.0
+var JUMP_FORCE = 650.0
 var velocity = Vector2()
-
+var jump_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 func _physics_process(delta):
-    if (!is_on_floor()):
-        velocity.y += GRAVITY * delta
-    if (Input.is_action_just_pressed("ui_up") && is_on_floor()):
-        velocity.y = -JUMP_FORCE
-        
-    move_and_slide(velocity, Vector2(0, -1))
+	if (!is_on_floor()):
+		velocity.y += GRAVITY# * delta
+	if (Input.is_action_just_pressed("ui_up") && is_on_floor()):
+		velocity.y = -JUMP_FORCE
+		jump_count = 1
+	if (Input.is_action_just_pressed("ui_up") && !is_on_floor()):
+		if jump_count < 2:
+			jump_count += 1
+			velocity.y = -JUMP_FORCE + 70
+		
+	move_and_slide(velocity, Vector2(0, -1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
