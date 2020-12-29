2be0218f289848b8a6bed27ce0b0117478356c60
Andre Kato <andrekato@usp.br>
2020-12-29 19:22:01 -0300

    Adds pause menu

--- GameJaajProject/Scenes/Menu/TitleScreen.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func start_game():
-    get_tree().change_scene("res://Scenes/World/World.tscn")
-
-
-func quit_game():
-    get_tree().quit()
--- GameJaajProject/Scenes/World/WorldUI.gd
+++ GameJaajProject/Scenes/World/WorldUI.gd
@@ -3,20 +3,22 @@ extends Control
 signal health_updated(health)
 
 onready var world = get_parent()
-var paused = false
+var paused = false setget set_pause
+onready var pause_overlay = get_node("CanvasLayer/PauseScreen")
 
 func _ready():
     var player = world.get_node("Player")
     get_node("CanvasLayer/HealthBar").initialize(player.hp, player.MAX_HP)
-    
-func _process(delta):
-    if (Input.is_action_just_pressed("ui_cancel")):
-        if (paused):
-            paused = false
-            world.resume_game()
-        else:
-            paused = true
-            world.pause_game()
 
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("ui_cancel"):
+        self.paused = !paused
+        get_tree().set_input_as_handled()
+
+func set_pause(value: bool) -> void:
+    paused = value
+    get_tree().paused = value
+    pause_overlay.visible = value
+    
 func _on_Player_health_changed(health):
     emit_signal("health_updated", health)
