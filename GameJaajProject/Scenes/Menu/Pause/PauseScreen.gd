a03b24045f30b6dc069776e507b704ca9f8d0c31
andrekato1 <andrekato@usp.br>
2021-01-02 16:58:56 -0300

    Adds title screen bgm and changes pause menu folder hierarchy

--- GameJaajProject/Scenes/Menu/Pause/PauseScreen.gd
+++ GameJaajProject/Scenes/Menu/Pause/PauseScreen.gd
@@ -1,18 +1,18 @@
 extends Control
 
-onready var grimoire_resource = preload("res://Scenes/Menu/Pause/Grimoire.tscn")
+onready var grimoire_resource = preload("res://Scenes/Menu/Pause/Grimoire/Grimoire.tscn")
 
 func _ready():
-	visible = false
+    visible = false
 
 func _on_ContinueButton_pressed():
-	get_tree().paused = false
-	visible = false
+    get_tree().paused = false
+    visible = false
 
 func _on_QuitButton_pressed():
-	Global.goto_scene("res://Scenes/Menu/Title/TitleScreen.tscn")
+    Global.goto_scene("res://Scenes/Menu/Title/TitleScreen.tscn")
 
 func _on_GrimoireButton_pressed():
-	var grimoire = grimoire_resource.instance()
-	add_child(grimoire)
-	$AnimatedSprite.play("open")
+    var grimoire = grimoire_resource.instance()
+    add_child(grimoire)
+    $AnimatedSprite.play("open")
