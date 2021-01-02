fb3c754a22ac47cfa2e294b13c594c7c2058183b
andrekato1 <andrekato@usp.br>
2021-01-02 17:57:08 -0300

    Adds a disable music option on title screen

--- GameJaajProject/Scenes/Menu/Title/TitleScreen.gd
+++ GameJaajProject/Scenes/Menu/Title/TitleScreen.gd
@@ -1,10 +1,20 @@
 extends Control
 
+onready var player = $"BGM"
+onready var animation = $"BGM/AnimationPlayer"
+
 func _ready():
-    $"BGM/AnimationPlayer".play("FadeIn")
+    animation.play("FadeIn")
 
 func start_game():
     Global.goto_scene("res://Scenes/World/World.tscn")
 
 func quit_game():
     get_tree().quit()
+
+func _on_SoundIcon_toggled(button_pressed):
+    if button_pressed:
+        player.stop()
+    else:
+        player.play()
+        animation.play("FadeIn")
