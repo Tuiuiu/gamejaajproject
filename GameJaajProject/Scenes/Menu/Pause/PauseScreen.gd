f5832b3ad617b4dccb986cd3749e83dc4e794f08
andrekato1 <andrekato@usp.br>
2021-01-02 23:01:41 -0300

    Creates grimoire pages for the fireballs

--- GameJaajProject/Scenes/Menu/Pause/PauseScreen.gd
+++ GameJaajProject/Scenes/Menu/Pause/PauseScreen.gd
@@ -1,9 +1,20 @@
 extends Control
 
 onready var grimoire_resource = preload("res://Scenes/Menu/Pause/Grimoire/Grimoire.tscn")
+onready var grimoire_pages: Array
 
 func _ready():
     visible = false
+    load_grimoire_pages()
+    
+func load_grimoire_pages():
+    var dir = Directory.new()
+    var path = "res://Scenes/Menu/Pause/Grimoire/Pages/Fireballs"
+    if dir.open(path) == OK:
+        dir.list_dir_begin()
+        var filename = dir.get_next()
+        while filename != "":
+            pass
 
 func _on_ContinueButton_pressed():
     get_tree().paused = false
@@ -16,3 +27,6 @@ func _on_GrimoireButton_pressed():
     var grimoire = grimoire_resource.instance()
     add_child(grimoire)
     $AnimatedSprite.play("open")
+    # open grimoire logic here
+    #$AnimatedSprite.play("close")
+    #remove_child(grimoire)
