ee5a5d81f3d6a4f8b285952ff90345fe0ec1ca97
Oliver-Belmont <arthurmarassatti@hotmail.com>
2021-01-03 21:27:27 -0300

    Adds DialogBox
    Adds DialogBox for cutscenes

---
+++ GameJaajProject/Scenes/Cutscenes/DialogBox.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var dialog = [["Vi sitter här I venten och spelar lite dota","velho brocha"],
+["I hear you man","lobinho"]]
+
+var active_actor = "A"
+var speak = 0
+func _ready():
+    load_dialog()
+
+func _process(delta):
+    if (Input.is_action_just_pressed("ui_accept")):
+        load_dialog()
+
+func load_dialog():
+    if (speak < dialog.size()):
+        $NinePatchRect/MarginContainer/Label.text = dialog[speak][0]
+        $NinePatchRect/NinePatchRect/SpeakerName.text = dialog[speak][1]
+        $NinePatchRect/MarginContainer/Label/AnimationPlayer.play("Write")
+        yield($NinePatchRect/MarginContainer/Label/AnimationPlayer, "animation_finished")
+        $NinePatchRect/NinePatchRect2/TextureButton/AnimationPlayer.play("Next")
+        speak += 1
+    else: queue_free()
+    
+
+
