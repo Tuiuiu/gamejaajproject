3e91dd809fa01a8243fac58975c305fb9b6b5665
Oliver-Belmont <arthurmarassatti@hotmail.com>
2021-01-04 13:11:14 -0300

    Adds first shop cutscene

--- GameJaajProject/Scenes/Cutscenes/DialogBox.gd
+++ GameJaajProject/Scenes/Cutscenes/DialogBox.gd
@@ -1,28 +1,28 @@
 extends Control
 
-var dialog = [["Vi sitter här I venten och spelar lite dota","velho brocha"],
-["I hear you man","lobinho"]]
+var dialog = [[]]
 
 
 var active_actor = "A"
-var speak = 0
-func _ready():
-    load_dialog()
+var speech = 0
+#func _ready():
+ #   load_dialog()
 
 func _process(delta):
     if (Input.is_action_just_pressed("ui_accept")):
+        $NinePatchRect/MarginContainer/Label.percent_visible = 0
         load_dialog()
 
 func load_dialog():
-    if (speak < dialog.size()):
-        $NinePatchRect/MarginContainer/Label.text = dialog[speak][0]
-        if (dialog[speak][1] != active_actor):
-            $NinePatchRect/NinePatchRect/SpeakerName.text = dialog[speak][1]
-            active_actor = dialog[speak][1]
+    if (speech < dialog.size()):
+        $NinePatchRect/MarginContainer/Label.text = dialog[speech][0]
+        if (dialog[speech][1] != active_actor):
+            $NinePatchRect/NinePatchRect/SpeakerName.text = dialog[speech][1]
+            active_actor = dialog[speech][1]
         $NinePatchRect/MarginContainer/Label/AnimationPlayer.play("Write")
         yield($NinePatchRect/MarginContainer/Label/AnimationPlayer, "animation_finished")
         $NinePatchRect/NinePatchRect2/TextureButton/AnimationPlayer.play("Next")
-        speak += 1
+        speech += 1
     else: queue_free()
     
 
--- GameJaajProject/Scenes/Cutscenes/ShopCutscene1.gd
+++ GameJaajProject/Scenes/Cutscenes/ShopCutscene1.gd
@@ -1,3 +1,16 @@
 extends Node2D
-#func _ready():
- #   $CanvasLayer/DialogBox.dialog = [["Escrever o conteúdo da cutscene","lucasgay"]]
+func _ready():
+    $CanvasLayer/DialogBox.dialog = [["Oh, a costumer! Welcome to my shop, feel free to browse.","???"],
+    ["What!? Who goes there!? (I can't see him).","protagonist"],
+    ["*sight* Above the crates, the furry little thing...Woof","???"],
+    ["Heavens, a talking wolf! I've never seen that kind of sorcery before!", "protagonist"],
+    ["It is a remarkable spell, indeed. However, I'm not the one responsible for it, as you may presume.","Wolf"],
+    ["This...curse was cast upon me a long time ago, when I came here to stop the summoning of a great evil.","Wolf"],
+    ["Wait, that would make you Eleanor, the...'Wolf' of the North...","protagonist"],
+    ["There is some irony to the title now, aye? *Sight*","Wolf"],
+    ["Kinda.","protagonist"],
+    ["But let us leave my failures to another time. Now, how can I help you, wizard?","Wolf"],
+    ["I'm in need of items with particularly 'unique' features. Is there something like that in your stock?",
+    "protagonist"],
+    ["Just look around, friend! Magical items, as unique as a talking wolf!","Wolf"]]
+    $CanvasLayer/DialogBox.load_dialog()
