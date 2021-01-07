76898492dca87757b0e98cdb48fdb9dc147bdc2c
Oliver-Belmont <arthurmarassatti@hotmail.com>
2021-01-07 18:41:37 -0300

    Adds content to most main scenes
    Stairs to World.tscn, Art and animation to TitleScreen.tscn, floor to ShopCutscene.tscn and much more.

---
+++ GameJaajProject/Scenes/Menu/Title/Background.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+func _ready():
+    while true:
+        $AquaPinkBG/AnimationPlayer.play("FadeOut")
+        yield($AquaPinkBG/AnimationPlayer, "animation_finished")
+        $BlueBG/AnimationPlayer2.play("FadeOut")
+        yield($BlueBG/AnimationPlayer2, "animation_finished")
+        $BlueBG/AnimationPlayer2.play("FadeIn")
+        yield($BlueBG/AnimationPlayer2, "animation_finished")
+        $AquaPinkBG/AnimationPlayer.play("FadeIn")
+        yield($AquaPinkBG/AnimationPlayer, "animation_finished")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
