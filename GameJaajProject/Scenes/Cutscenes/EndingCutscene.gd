51070cc0ec7d6a7aa534807e5f98c422154947b6
Oliver-Belmont <arthurmarassatti@hotmail.com>
2021-01-08 00:25:46 -0300

    Cutscene changes

---
+++ GameJaajProject/Scenes/Cutscenes/EndingCutscene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _ready():
+    $DialogBox.dialog=[["Brother!!", "Lumorith"],
+    ["...","Black Mage"],
+    ["Zahr, it's me! We gotta talk to the Circle, they think you...", "Lumorith"],
+    [""]]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
