794300ee1fd2f1cd3ece2eda22aa845255f2cb63
Oliver-Belmont <arthurmarassatti@hotmail.com>
2021-01-09 20:21:21 -0300

    Adds Shop Cutscenes
    Also updates TitleScreen

--- GameJaajProject/Scenes/Cutscenes/ShopCutscene1.gd
+++ GameJaajProject/Scenes/Cutscenes/ShopCutscene1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 func _ready():
     $CanvasLayer/DialogBox.dialog = [["Oh, a costumer! Welcome to my shop, feel free to browse.","???"],
-    ["What!? Who goes there!? (I can't see him).","protagonist"],
+    ["What!? Who goes there!? (I can't see him).","Lumorith"],
     ["*sight* Above the crates, the furry little thing...Woof","???"],
     ["Heavens, a talking wolf! I've never seen that kind of sorcery before!", "Lumorith"],
     ["It is a remarkable spell, indeed. However, I'm not the one responsible for it, as you may presume.","Wolf"],
@@ -12,5 +12,11 @@ func _ready():
     ["But let us leave my failures to another time. Now, how can I help you, wizard?","Wolf"],
     ["I'm in need of items with particularly 'unique' features. Is there something like that in your stock?",
     "Lumorith"],
-    ["Just look around, friend! Magical items, as unique as a talking wolf!","Wolf"]]
+    ["Just look around, friend! Magical items, as unique as a talking wolf!","Wolf"],
+    ["These scrolls. They seem to contain a magic unkown to me...", "Lumorith"],
+    ["Those are useless to me in my current condition. Feel free to transcribe them to your grimoire if you want to.", "Wolf"],
+    ["T-thank you Mrs.Eleanor, you have my eternal gratitude!","Lumorith"],
+    ["Lumorith learned two new spells!",""],
+    ["Press BFB for Black Fireball and GFB for Green Fireball when facing monsters", ""],
+    ["*Woof* (Remember to check your grimoire later to learn about your new spells) *woof*", "Wolf"]]
     $CanvasLayer/DialogBox.load_dialog()
