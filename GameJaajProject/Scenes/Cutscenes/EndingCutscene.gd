b2e1541455d6caa6992e13a75eeda4cf4e8a5bc3
Oliver-Belmont <arthurmarassatti@hotmail.com>
2021-01-08 23:20:22 -0300

    Boss Fight Intro Cutscene
    Adds boss fight intro cutscene, needs review on script to fade out properly

--- GameJaajProject/Scenes/Cutscenes/EndingCutscene.gd
+++ GameJaajProject/Scenes/Cutscenes/EndingCutscene.gd
@@ -1,12 +1,39 @@
 extends Node2D
-
+var foi = false
+var pode = true
 
 func _ready():
+    $AudioStreamPlayer.play()
     $DialogBox.dialog=[["Brother!!", "Lumorith"],
     ["...","Black Mage"],
-    ["Zahr, it's me! We gotta talk to the Circle, they think you...", "Lumorith"],
-    [""]]
+    ["Zahr, it's me! We must talk to the Circle, they think you've...", "Lumorith"],
+    ]
+    $DialogBox.visible = false
+    $PlayerPosition/AnimationPlayer.play("Arrive")
+    yield($PlayerPosition/AnimationPlayer, "animation_finished")
+    $PlayerPosition/CutscenePlayer.play("Idle")
+    $DialogBox.load_dialog()
+    $DialogBox.visible = true
 
+func _process(_delta):
+    if ($DialogBox.speech == 3 and (Input.is_action_just_pressed("ui_accept")) and !foi and pode):
+        foi = true
+        $BlackMage.play("Attack1")
+        yield($BlackMage, "animation_finished")
+        $BlackMage.play("Idle")
+        $PlayerPosition/CutscenePlayer.play("Hit")
+        $PlayerPosition/AnimationPlayer.play("Fly")
+        yield($PlayerPosition/AnimationPlayer, "animation_finished")
+        $PlayerPosition/CutscenePlayer.play("Idle")
+        $DialogBox.dialog = [["Zahr!?", "Lumorith"],
+        ["*sigh* Oh, my brother, were they right all along?", "Lumorith"],
+        ["I am... so sorry.", "Lumorith"]]
+        $DialogBox.visible = true
+        #final_fade()
+
+#func final_fade():
+    #$ColorRect/AnimationPlayer.play("Fade")
+    #yield($ColorRect/AnimationPlayer, "animation_finished")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
